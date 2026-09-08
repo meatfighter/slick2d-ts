d80b202d6fe44e4165a8226e11e128dd99033094
zeroone <54445200+meatfighter@users.noreply.github.com>
2026-09-08 15:58:22 -0400

    Gate sound playback on Web Audio resume

--- src/slick/openal/SoundStore.ts
+++ src/slick/openal/SoundStore.ts
@@ -1,3 +1,4 @@
+import { AudioContextLifecycle } from "./AudioContextLifecycle.js";
 import { ResourceLoadException, ResourceLoader, type ResourceLoadOptions } from "../util/ResourceLoader.js";
 import { runSettledBatch } from "../util/BatchLoader.js";
 import { Log } from "../util/Log.js";
@@ -353,13 +354,11 @@ export class SoundStore {
             this.musicEnabled = true;
             this.resetSoundSources();
         }
-        try {
-            await context.resume?.();
-            return context.state !== "closed";
-        } catch (error) {
-            Log.warn("Unable to unlock Web Audio", error);
-            return false;
+        const resumed = await AudioContextLifecycle.resume(context);
+        if (!resumed) {
+            Log.warn("Unable to unlock Web Audio");
         }
+        return resumed;
     }
 
     /** Browser parity helper: returns the global sound-effect gain bus. */
@@ -571,11 +570,16 @@ export class SoundStore {
         this.activeHandles.add(handle);
         this.soundSources[sourceId] = handle;
         void this.loadAudioBuffer(ref)
-            .then((buffer) => {
+            .then(async (buffer) => {
                 if (stopped) {
                     return;
                 }
-                void context.resume().catch(() => undefined);
+                if (!(await AudioContextLifecycle.resume(context)) || stopped || !this.soundsEnabled) {
+                    if (!stopped) {
+                        handle.stop();
+                    }
+                    return;
+                }
                 gain = context.createGain();
                 source = context.createBufferSource();
                 source.buffer = buffer;
